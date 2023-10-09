5b39c1c1e024cc772cf467692fcd936b157fb87f
Guram12 <guram.shanidze.33@gmail.com>
2023-10-09 22:23:00 +0400

    edded language translations for header element

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,61 @@
-import './Header.css'
-
+import React, { useState } from 'react';
+import './Header.css';
 
 export function Header() {
+    const [language, setLanguage] = useState('en');
+
+    const content = {
+        en: {
+            home: 'HOME',
+            company: 'Company',
+            services: 'Services',
+            projects: 'Projects',
+            contact: 'Contact'
+        },
+        es: {
+            home: 'INICIO',
+            company: 'Empresa',
+            services: 'Servicios',
+            projects: 'Proyectos',
+            contact: 'Contacto'
+        },
+        ka: { // Added Georgian translations
+            home: 'მთავარი',
+            company: 'კომპანია',
+            services: 'მომსახურებები',
+            projects: 'პროექტები',
+            contact: 'კონტაქტი'
+        }
+    }
+
+    const toggleLanguage = () => {
+        setLanguage((prevLanguage) => {
+            switch (prevLanguage) {
+                case 'en':
+                    return 'es';
+                case 'es':
+                    return 'ka';
+                case 'ka':
+                    return 'en';
+                default:
+                    return 'en';
+            }
+        });
+    }
+
     return (
         <header className='header'>
             <img src='./company_logo.png' alt='elside company logo' className='logo' />
-            <div class="container">
-                <div className="btn"><a href="one">HOME</a></div>
-                <div className="btn"><a href="two" >Company</a></div>
-                <div className="btn"><a href="three" >Services</a></div>
-                <div className="btn"><a href="four" >Projects</a></div>
-                <div className="btn"><a href="five" >Contact</a></div>
-	        </div>
+            <div className="container">
+                <div className="btn"><a href="one">{content[language].home}</a></div>
+                <div className="btn"><a href="two">{content[language].company}</a></div>
+                <div className="btn"><a href="three">{content[language].services}</a></div>
+                <div className="btn"><a href="four">{content[language].projects}</a></div>
+                <div className="btn"><a href="five">{content[language].contact}</a></div>
+                <button onClick={toggleLanguage} className="language-btn">
+                    {language === 'en' ? 'Español' : language === 'es' ? 'ქართული' : 'English'}
+                </button>
+            </div>
         </header>
     )
 }
