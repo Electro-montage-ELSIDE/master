cfc573d5766c9209ac70632fb769df4fea132145
Guram12 <guram.shanidze.33@gmail.com>
2023-10-17 23:36:27 +0400

    edded corrent project video styles

--- src/pages/CorrentprojectPage.js
+++ src/pages/CorrentprojectPage.js
@@ -1,30 +1,30 @@
-import './CorrentprojectPage.css'
+import './CorrentprojectPage.css';
 import ReactPlayer from 'react-player';
-import video from '../videos/video2.mp4'
+import video from '../videos/video2.mp4';
 
 export default function OngoingPage() {
-    return(
-        <div className='corrent_test' >
-            <div className='corrent_project_content'  >
+    return (
+        <div className='corrent_test'>
+            <div className='corrent_project_content'>
                 <div>
-                    <h1> corrent project's content goes here</h1>
+                    <h1>Current project's content goes here</h1>
                 </div>
-                <div className='corrent_video_1' >
+                
+                <div className="vertical_line"></div> {/* Add this for the vertical line */}
+
+                <div className='corrent_video_1'>
                     <ReactPlayer
                         className="correntprojectfirst"
                         url={video}
                         playing={true}
-                        controls={false}
+                        controls={true} 
                         loop={true}
                         width='100%'
                         height='100%'
-                        volume={false}
-                    
+                        volume={0}
                     />
-                   
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
